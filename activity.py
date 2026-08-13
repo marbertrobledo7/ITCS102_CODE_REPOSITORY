6ceb9424095e80a01205d337921a15221617b2c5
marbertrobledo7 <marbertrobledo7@gmail.com>
2026-08-13 12:59:48 +0800

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,7 @@
+#hello world hello pilipins
+#kung ako sana pinili mo eh
+#i miss you
+
+print("hello world")
+print("ako nalang sana")
+print("miss you")
---
+++ activity2.py
@@ -0,0 +1,16 @@
+# integer variable 
+age = 17
+
+# float variable
+myWeight = 70
+
+# String
+myName = "Marbert Mapagmahal Robledo"
+
+# BOOLEAN VARIABLE
+ismyfriendspongebob = True
+
+print("my age is", age)
+print("my Name is", myName)
+print("is my friend spongebob", ismyfriendspongebob)
+print("my weight", myWeight)
