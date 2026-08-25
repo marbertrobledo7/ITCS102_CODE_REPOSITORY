b6e3c8ad55dab40408a133a07cfb17d2cc87adaa
marbertrobledo7 <marbertrobledo7@gmail.com>
2026-08-25 12:13:34 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,11 @@
+a=67
+b=76
+sum=a+b
+diff=a-b
+print("The sum of ",a,"plus",b,"is", sum)
+print("The sum of ",a,"minus",b,"is", a-b)
+print("The sum of ",a,"times",b,"is", a*b)
+print("The sum of ",a,"division",b,"is", a/b)
+print("The sum of ",a,"floor_division",b,"is", a//b)
+print("The sum of ",a,"exponent",b,"is", a**b)
+print("The sum of ",a,"modules",b,"is", a%b)
