2afbf992f051f4dff9bb06d744a95b3c34a76018
marbertrobledo7 <marbertrobledo7@gmail.com>
2026-09-03 12:41:59 +0800

    Update activity3.py

---
+++ activity3.py
@@ -0,0 +1,21 @@
+male = 14
+female = 25
+chair = 50
+
+
+float
+my height= 165.4
+temperature= 58.9
+grade = 99.9
+
+mynamne= "Marbert Robledo"
+mymom="marilou robledo"
+mycat= "adolf hitler"
+
+isinlove= true
+ishaveacat= true
+ishaveafather= true
+
+print("Hi my name is ",myname)
+print("Hi my mom is ", mymom)
+print("our cat name is ", mycat)
