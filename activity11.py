a35ebe32a1cc4de7c8fed5f68f2421f3953a6418
marbertrobledo7 <marbertrobledo7@gmail.com>
2026-09-08 12:33:58 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,10 @@
+username= 'marbertlovesclaire'
+password= 'lovesyouasalways'
+
+u = input('input username-->')
+p = input('input password-->')
+
+if username == u and p == password:
+       print("ACCESS GRANTED")
+else:
+       print("engggk ENGOTT!!!")
