dd098107ce04ab2b50a18f77ab337d57c70eb25d
marbertrobledo7 <marbertrobledo7@gmail.com>
2026-09-08 12:42:09 +0800

    Add files via upload

---
+++ activity12.py
@@ -0,0 +1,12 @@
+import getpass
+
+username= 'marbertlovesclaire'
+password= 'lovesyouasalways'
+
+u = input('input username-->')
+p = getpass.getpass('input password-->')
+
+if username == u and p == password:
+       print("ACCESS GRANTED")
+else:
+       print("engggk ENGOTT!!!")
