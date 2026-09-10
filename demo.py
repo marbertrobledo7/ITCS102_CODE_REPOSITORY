dbd87a24ada261c953c6d2317330b11b8ba71909
marbertrobledo7 <marbertrobledo7@gmail.com>
2026-09-10 14:37:55 +0800

    demo.py

--- demo.py
+++ demo.py
@@ -12,8 +12,23 @@ is_express = bool(input("The item is rush:" ))
 
 is_international = bool(input("Is this going to be taken internationally:" ))
 
-if weight <= 2:
-   print("the shipping will be free")
+base_cost = (weight * 2.50) + (distance * 0.15)
+
+if weight <= 2 and distance <= 100 and not is_express and not is_international:
+    total = 0
+
+elif is_international and is_express:
+    total = (base_cost * 1.40) + 50
+
+elif is_express or (is_international and weight > 20):
+    total = (base_cost * 1.20) + 25
+
+elif weight > 30 or distance > 1000:
+    total = base_cost + 30
+
+else:
+    total = base_cost
+
 
 
 
